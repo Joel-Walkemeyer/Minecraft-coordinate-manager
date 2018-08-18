60b760af4c02bf04edcdf596dc29f210fd102028
Joel-Walkemeyer <37867089+Joel-Walkemeyer@users.noreply.github.com>
2018-08-18 17:06:08 +0800

    organise home form

--- ListView/Program.cs
+++ ListView/Program.cs
@@ -17,37 +17,47 @@ namespace CoordinateManager
         /// </summary>
 
         public static List<World> worlds = new List<World>();
-        static readonly string pth = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\.minecraft";
+        static string pth;
 
         [STAThread]
         static void Main()
         {
-            string[] d = Directory.GetDirectories(pth + @"\saves");
+            pth = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ".minecraft");
+
+            ReadWorlds();
+            ReadServers();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Home());
+        }
+
+        private static void ReadWorlds()
+        {
+            string[] d = Directory.GetDirectories(Path.Combine(pth, @"saves"));
 
             foreach (string dir in d)
             {
-                if (File.Exists(dir + @"\level.dat"))
+                if (File.Exists(Path.Combine(dir, @"level.dat")))
                 {
                     NbtFile worldFile = new NbtFile();
-                    worldFile.LoadFromFile(dir + @"\level.dat");
+                    worldFile.LoadFromFile(Path.Combine(dir, @"level.dat"));
                     var tag = worldFile.RootTag;
                     string name = (tag.Get<NbtCompound>("Data").Get<NbtString>("LevelName").Value.ToString());
                     worlds.Add(new World(dir, name, World.type.singlePlayer));
                 }
-
-
-
             }
+        }
 
-            if (!Directory.Exists(pth + @"\saves\coords"))
+        private static void ReadServers()
+        {
+            if (!Directory.Exists(Path.Combine(pth + @"saves\coords")))
             {
-                Directory.CreateDirectory(pth + @"\saves\coords");
+                Directory.CreateDirectory(Path.Combine(pth + @"saves\coords"));
             }
-
-
 
             NbtFile serverFile = new NbtFile();
-            serverFile.LoadFromFile(pth + @"\servers.dat");
+            serverFile.LoadFromFile(Path.Combine(pth, "servers.dat"));
 
             var servers = serverFile.RootTag.Get<NbtList>("servers");
 
@@ -55,15 +65,8 @@ namespace CoordinateManager
             {
                 string serverName;
                 serverName = servers.Get<NbtCompound>(i).Get<NbtString>("name").Value.ToString();
-                worlds.Add(new World(pth + @"\saves\coords\" + serverName + @".txt", serverName, World.type.server));
+                worlds.Add(new World(Path.Combine(pth, @"saves\coords\", serverName + @".txt"), serverName, World.type.server));
             }
-                
-            
-
-
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Home());
         }
     }
 }
